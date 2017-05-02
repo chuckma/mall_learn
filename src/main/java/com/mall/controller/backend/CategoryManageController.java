16ade26cfa330ea5f9b0fb3a4cd43012d9cf2b3f
chuckma <robbincen@163.com>
2017-05-02 20:04:17 +0800

    分类完成

--- src/main/java/com/mall/controller/backend/CategoryManageController.java
+++ src/main/java/com/mall/controller/backend/CategoryManageController.java
@@ -1,8 +1,120 @@
 package com.mall.controller.backend;
 
+import com.mall.common.Const;
+import com.mall.common.ResponseCode;
+import com.mall.common.ServerResponse;
+import com.mall.pojo.User;
+import com.mall.service.ICategoryService;
+import com.mall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
 /**
- * \* Created by chuck Date:2017/4/27
- * \
+ * Created by chuck Date:2017/4/27
  */
+@Controller
+@RequestMapping("manage/category")
 public class CategoryManageController {
+
+
+    @Autowired
+    private IUserService iUserService;
+
+    @Autowired
+    private ICategoryService iCategoryService;
+
+
+    /**
+     * 添加产品分类
+     *
+     * @param session
+     * @param categoryName
+     * @param parentId
+     * @return
+     */
+    @RequestMapping("add_category.do")
+    @ResponseBody
+    public ServerResponse addCategory(HttpSession session, String categoryName, @RequestParam(value = "parentId", defaultValue = "0") int parentId) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录,请登录!");
+        }
+        // 校验是否是管理员
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            // 是管理员
+            // 增加处理分类的逻辑
+            return iCategoryService.addCategory(categoryName, parentId);
+        } else {
+            return ServerResponse.createByErrorMessage("无权限操作,需要管理员权限");
+        }
+    }
+
+    /**
+     * 设置分类信息
+     * @param session
+     * @param categoryId
+     * @param categoryName
+     * @return
+     */
+    @RequestMapping("set_category_name.do")
+    @ResponseBody
+    public ServerResponse setCategoryName(HttpSession session, Integer categoryId, String categoryName) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录,请登录!");
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            // 更新categoreName
+            return iCategoryService.updateCategory(categoryId, categoryName);
+        } else {
+            return ServerResponse.createByErrorMessage("无权限操作,需要管理员权限");
+        }
+    }
+
+
+
+    @RequestMapping("get_category.do")
+    @ResponseBody
+    public ServerResponse getChilrenParallelCategory(HttpSession session,@RequestParam(value = "categoryId",defaultValue = "0") Integer categoryId){
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录,请登录!");
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+
+            // 查询子节点的category信息,并且不递归,保持平级
+            return iCategoryService.getChildrenParallelCategory(categoryId);
+        } else {
+            return ServerResponse.createByErrorMessage("无权限操作,需要管理员权限");
+        }
+    }
+
+    /**
+     * 深度查询分类
+     * @param session
+     * @param categoryId
+     * @return
+     */
+    @RequestMapping("get_deep_category.do")
+    @ResponseBody
+    public ServerResponse getCategoryAndDeepChildrenCategory(HttpSession session,@RequestParam(value = "categoryId",defaultValue = "0") Integer categoryId){
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录,请登录!");
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            // 查询当前节点的ID和递归子节点的ID [0->10000->100000]
+            return iCategoryService.selectCategoryAndChilrenById(categoryId);
+        } else {
+            return ServerResponse.createByErrorMessage("无权限操作,需要管理员权限");
+        }
+    }
+
+
+
 }
