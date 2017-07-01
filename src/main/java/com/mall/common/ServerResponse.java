720fe1473d27bc0329bc86db563afaeadb81afcd
chuckma <robbincen@163.com>
2017-07-01 11:06:55 +0800

    格式化ServerResponse

--- src/main/java/com/mall/common/ServerResponse.java
+++ src/main/java/com/mall/common/ServerResponse.java
@@ -17,15 +17,16 @@ public class ServerResponse<T> implements Serializable {
     private String msg;
     private T data; // 泛型返回的时候可以指定里面的内容,也可以不指定里面的类型
 
-    private ServerResponse(int status){
+    private ServerResponse(int status) {
         this.status = status;
     }
+
     private ServerResponse(int status, T data) {
         this.status = status;
         this.data = data;
     }
 
-    private ServerResponse(int status, String msg,T data) {
+    private ServerResponse(int status, String msg, T data) {
         this.status = status;
         this.msg = msg;
         this.data = data;
@@ -37,7 +38,7 @@ public class ServerResponse<T> implements Serializable {
     }
 
     @JsonIgnore  // 序列化的时候不会显示在json 中
-    public boolean isSuccess(){
+    public boolean isSuccess() {
         return this.status == ResponseCode.SUCCESS.getCode();
     }
 
@@ -53,31 +54,31 @@ public class ServerResponse<T> implements Serializable {
         return msg;
     }
 
-    public static <T> ServerResponse<T> createBySuccess(){
+    public static <T> ServerResponse<T> createBySuccess() {
         return new ServerResponse<T>(ResponseCode.SUCCESS.getCode());
     }
 
-    public static <T> ServerResponse<T> createBySuccessMessage(String msg){
-        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),msg);
+    public static <T> ServerResponse<T> createBySuccessMessage(String msg) {
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), msg);
     }
 
-    public static <T> ServerResponse<T> createBySuccess(T data){
-        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),data);
+    public static <T> ServerResponse<T> createBySuccess(T data) {
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), data);
     }
 
-    public static <T> ServerResponse<T> createBySuccess(String msg,T data){
-        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),msg,data);
+    public static <T> ServerResponse<T> createBySuccess(String msg, T data) {
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), msg, data);
     }
 
-    public static <T>ServerResponse<T> createByError(){
-        return new ServerResponse<T>(ResponseCode.ERROR.getCode(),ResponseCode.ERROR.getDesc());
+    public static <T> ServerResponse<T> createByError() {
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(), ResponseCode.ERROR.getDesc());
     }
 
-    public static <T>ServerResponse<T> createByErrorMessage(String errorMessage){
-        return new ServerResponse<T>(ResponseCode.ERROR.getCode(),errorMessage);
+    public static <T> ServerResponse<T> createByErrorMessage(String errorMessage) {
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(), errorMessage);
     }
 
-    public static <T>ServerResponse<T> createByErrorCodeMessage(int errorCode,String errorMessage){
-        return new ServerResponse<T>(errorCode,errorMessage);
+    public static <T> ServerResponse<T> createByErrorCodeMessage(int errorCode, String errorMessage) {
+        return new ServerResponse<T>(errorCode, errorMessage);
     }
 }
