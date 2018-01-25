0c52e24c7cc90692d5c803484e504aaeda0a2d71
chuckma <robbincen@163.com>
2018-01-25 14:03:09 +0800

    add cookieUtil

---
+++ src/main/java/com/mall/util/CookieUtil.java
@@ -0,0 +1,74 @@
+package com.mall.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class CookieUtil {
+
+    private final static String COOKIE_DOMAIN = "";
+    private final static String COOKIE_NAME = "mmall_login_token";
+
+    /**
+     * 读取cookie
+     * @param request
+     * @return
+     */
+    public static String readLoginToken(HttpServletRequest request) {
+        Cookie[] cks = request.getCookies();
+        if (cks != null) {
+            for (Cookie ck : cks) {
+                log.info("read cookieName:{},cookieValue:{}", ck.getName(), ck.getValue());
+                if (StringUtils.equals(ck.getName(), COOKIE_NAME)) {
+                    log.info("return cookieName:{},cookieValue:{}", ck.getName(), ck.getValue());
+                    return ck.getValue();
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * 写入cookie
+     *
+     * @param response
+     * @param token
+     */
+    public static void writeLoginToken(HttpServletResponse response, String token) {
+
+        Cookie ck = new Cookie(COOKIE_NAME, token);
+        ck.setDomain(COOKIE_DOMAIN);
+        ck.setPath("/");    //代表设置在根目录
+        //单位是秒
+        // 如果maxage 不设置cookie 就不会写入硬盘,而是写入内存, 只在当前页面有效
+        ck.setMaxAge(60 * 60 * 24 * 365); // 如果是-1 代表永久;
+        log.info("write cookieName:{},cookieValue:{}", ck.getName(), ck.getValue());
+        response.addCookie(ck);
+    }
+
+    /**
+     * 删除 cookie
+     * @param request
+     * @param response
+     */
+    public static void delLoginToken(HttpServletRequest request, HttpServletResponse response) {
+        Cookie [] cks = request.getCookies();
+        if (cks != null) {
+            for (Cookie ck : cks) {
+                if (StringUtils.equals(ck.getName(), COOKIE_NAME)) {
+                    ck.setDomain(COOKIE_DOMAIN);
+                    ck.setPath("/");
+                    ck.setMaxAge(0);    // 0 代表删除 cookie
+                    log.info("delete cookieName:{},cookieValue:{}", ck.getName(), ck.getValue());
+                    response.addCookie(ck);
+                    return;
+                }
+            }
+        }
+    }
+}
