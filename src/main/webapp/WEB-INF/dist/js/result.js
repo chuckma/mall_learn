55035514c005653c0bd8cde6df1579c94e1ff35d
chuckma <robbincen@163.com>
2018-04-07 15:30:30 +0800

    deploy fre

---
+++ src/main/webapp/WEB-INF/dist/js/result.js
@@ -0,0 +1,1 @@
+webpackJsonp([11],{0:function(t,n,c){t.exports=c(50)},9:function(t,n){},12:function(t,n,c){"use strict";c(9)},21:function(t,n){},50:function(t,n,c){"use strict";c(21),c(12);var s=c(1);$(function(){var t=s.getUrlParam("type")||"default",n=$("."+t+"-success");n.show()})}});
